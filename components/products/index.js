b2444faa56e5b116fe8965cb1ce8d6e614e34eb1
Kennybabs-py <babalolakehinde74@gmail.com>
2022-12-07 20:10:21 +0100

    products page setup

---
+++ components/products/index.js
@@ -0,0 +1,11 @@
+import styles from "./products.module.css";
+
+export default function Products() {
+  return (
+    <main className={styles.products}>
+      <div className={styles.container}>
+        <h2 className={styles.header}>Products</h2>
+      </div>
+    </main>
+  );
+}
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,5 @@
-import React from "react";
-import styles from "../../styles/Home.module.css";
+import Products from "../../components/products";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <h1>Products</h1>
-      </div>
-    </main>
-  );
+  return <Products />;
 }
